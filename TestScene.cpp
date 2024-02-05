cee1a772e6ffe2096d8f08eb0c54438d08482c5f
悠人 菅原 <220369@jc-21.jp>
2024-02-05 17:05:36 +0900

    //Rectの判定終了

--- TestScene.cpp
+++ TestScene.cpp
@@ -2,6 +2,7 @@
 
 #include "Rect.h"
 #include "Engine/Input.h"
+#include "Engine/ImGui/imgui.h"
 
 TestScene::TestScene(GameObject * parent)
 	: GameObject(parent, "TestScene")
@@ -9,13 +10,23 @@ TestScene::TestScene(GameObject * parent)
 }
 
 void TestScene::Initialize()
-{
-	XMFLOAT3 mPos = Input::GetMousePosition();
-
+{	
 }
 
 void TestScene::Update()
 {
+	XMFLOAT3 mPos = Input::GetMousePosition();
+	ImGui::Text("mousePos = %f,%f", mPos.x, mPos.y);
+
+	Rect rect(640.f, 360.f, 100.f, 150.f);
+	
+	if (rect.ContainsPoint(mPos.x, mPos.y)) {
+		ImGui::Text("rect.ContainsPoint = true");
+	}
+	else {
+		ImGui::Text("rect.ContainsPoint = false");
+	}
+
 }
 
 void TestScene::Draw()
