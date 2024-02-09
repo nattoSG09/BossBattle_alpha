1f8048ae3a259157f4c17805fb7ffab2efe890d4
悠人 菅原 <220369@jc-21.jp>
2024-02-09 13:45:01 +0900

    //aha

--- Ore.cpp
+++ Ore.cpp
@@ -13,7 +13,6 @@ void Ore::Initialize()
 	hModel_ = Model::Load("Models/iron_ore.fbx");
 	//transform_.scale_ = { 2.f,2.f, 2.f };
 	transform_.rotate_.y = 90;
-	transform_.position_ = { 2.f,0.f,5.f };
 }
 
 void Ore::Update()
--- TestScene.cpp
+++ TestScene.cpp
@@ -1,7 +1,7 @@
 #include "TestScene.h"
 #include "Engine/ImGui/imgui.h"
 #include "Engine/Camera.h"
-
+#include<iostream>
 #include "Stage.h"
 #include "Player.h"
 #include "Ore.h"
@@ -13,10 +13,14 @@ TestScene::TestScene(GameObject * parent)
 
 void TestScene::Initialize()
 {	
+
 	// カメラの設定
 	Camera::SetPosition(XMFLOAT3(0, 10, -20));
 	Camera::SetTarget(XMFLOAT3(0, 7, 0));
 	// 作りたいゲーム・・・ない
+
+	// 乱数の初期化
+	srand((unsigned int)time(nullptr));
 
 	// 必要なGameObject
 	{
@@ -24,7 +28,10 @@ void TestScene::Initialize()
 		Instantiate<Player>(this);
 
 		// 鉱石
-		Instantiate<Ore>(this);
+		for (int i = 0; i < 3; i++) {
+			Ore* ore = Instantiate<Ore>(this);
+			ore->SetPosition(rand() % 20 + 1, 0, rand() % 20 + 1);
+		}
 
 		// ステージ
 		Instantiate<Stage>(this);
