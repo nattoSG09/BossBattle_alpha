7ce7a296462d4853c4d78dd59cceaa2071ab4f67
悠人 菅原 <220369@jc-21.jp>
2024-02-20 15:08:30 +0900

    da

--- Player.cpp
+++ Player.cpp
@@ -9,7 +9,7 @@
 
 namespace {
     const float sensitivity = 0.5f;// マウス感度
-    const float playerCameraDistance = 10.f;
+    const float playerCameraDistance = 5.f;
     const float playerHeadHeight = 4.f;
 }
 
@@ -31,7 +31,7 @@ void Player::Update()
 	// debug情報の表示
 	{
 		XMFLOAT3 pPos = transform_.position_;
-		ImGui::Text("Player.position = { x(%f),y(%f),z(%f) }", pPos.x, pPos.y, pPos.z);
+        ImGui::Text("Player.position = %f,%f,%f", pPos.x, pPos.y, pPos.z);
 	}
 	
 	// 移動処理&アニメーション処理
@@ -109,7 +109,7 @@ void Player::Update()
 
             // 回転行列をマウスの移動量を基に作成
             XMMATRIX matRotate = 
-                XMMatrixRotationX(XMConvertToRadians(mouseMove.y * sensitivity)) * XMMatrixRotationY(XMConvertToRadians(mouseMove.x * sensitivity));
+                /*XMMatrixRotationX(XMConvertToRadians(mouseMove.y * sensitivity)) **/ XMMatrixRotationY(XMConvertToRadians(mouseMove.x * sensitivity));
 
             // 回転行列を掛けて、向きベクトルを回転
             player_To_camPos = XMVector3Transform(player_To_camPos, matRotate);
@@ -128,30 +128,32 @@ void Player::Update()
         {
 
             // 正規化済みの向きベクトルを用意
-            XMVECTOR player_To_camPos = XMLoadFloat3(&camera_target) - XMLoadFloat3(&playerHead_position);
+            XMVECTOR player_To_camPos = XMLoadFloat3(&camera_position) - XMLoadFloat3(&playerHead_position);
             player_To_camPos = XMVector3Normalize(player_To_camPos);
 
             // 回転行列をマウスの移動量を基に作成
             XMMATRIX matRotate =
-                XMMatrixRotationX(XMConvertToRadians(mouseMove.y * sensitivity) + 45.f) * XMMatrixRotationY(XMConvertToRadians(mouseMove.x * sensitivity) + 45.f);
+                /*XMMatrixRotationX(XMConvertToRadians(mouseMove.y * sensitivity) + 45.f) **/ XMMatrixRotationY(XMConvertToRadians(mouseMove.x * sensitivity) + 45.f);
 
             // 回転行列を掛けて、向きベクトルを回転
-            player_To_camPos = XMVector3Transform(player_To_camPos, matRotate);
+            XMVECTOR player_To_camTgt = XMVector3Transform(player_To_camPos, matRotate);
 
             // 長さを変更
-            player_To_camPos *= playerCameraDistance;
+            player_To_camTgt *= playerCameraDistance;
 
             // 原点０，０から回転後のカメラの位置に伸びるベクトルを作成し、位置に代入
-            XMVECTOR origin_To_camPos = player_To_camPos + XMLoadFloat3(&playerHead_position);
-            XMStoreFloat3(&camera_target, origin_To_camPos);
+            XMVECTOR origin_To_camTgt = XMLoadFloat3(&playerHead_position)- player_To_camTgt;
+            XMStoreFloat3(&camera_target, origin_To_camTgt);
 
         }
 
+        ImGui::Text("camera_position = %f,%f,%f", camera_position.x, camera_position.y, camera_position.z);
         Camera::SetPosition(camera_position);
 
         // 焦点が頭部の時はうまくいく。
         //Camera::SetTarget(playerHead_position);
 
+        ImGui::Text("camera_target = %f,%f,%f", camera_target.x, camera_target.y, camera_target.z);
         Camera::SetTarget(camera_target);
 
     }
