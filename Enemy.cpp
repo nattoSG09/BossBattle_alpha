2dfa201d729dd598242fc49c816e3efd937670a7
悠人 菅原 <220369@jc-21.jp>
2024-02-21 13:28:18 +0900

    kf

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,6 +1,47 @@
 #include "Enemy.h"
 #include "Engine/Model.h"
-#include "Engine/CsvReader.h"
+#include <map>
+#include <fstream>
+#include <sstream>
+
+namespace {
+	
+    // .ini ファイルからモンスターの情報を読み込む関数
+    std::map<std::string, MonsterInfo> LoadMonsterInfo(const std::string& filename) {
+        std::map<std::string, MonsterInfo> monsterMap;
+
+        std::ifstream file(filename);
+        if (!file.is_open()) return monsterMap;
+        
+        std::string line;
+        std::string currentSection;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::string key, value;
+            if (std::getline(iss, key, '=')) {
+                if (key[0] == '[') {
+                    currentSection = key.substr(1, key.size() - 2);
+                    monsterMap[currentSection] = MonsterInfo();
+                }
+                else if (std::getline(iss, value)) {
+                    if (currentSection.empty()) {
+                        return monsterMap;
+                    }
+                    if (key == "filePath ") {
+                        monsterMap[currentSection].filePath = value;
+                    }
+                    else if (key == "speed ") {
+                        monsterMap[currentSection].speed = std::stoi(value);
+                    }
+                    else if (key == "guts ") {
+                        monsterMap[currentSection].guts = std::stoi(value);
+                    }
+                }
+            }
+        }
+        return monsterMap;
+    }
+}
 
 Enemy::Enemy(GameObject* parent)
 	:GameObject(parent,"Enemy")
@@ -9,10 +50,9 @@ Enemy::Enemy(GameObject* parent)
 
 void Enemy::Initialize()
 {
-	CsvReader csv;
-	csv.Load("OreMonsterData.csv");
+    
 
-	hModel_ = Model::Load("Models/OreTyrantrolling.fbx");
+	hModel_ = Model::Load();
 	Model::SetAnimFrame(hModel_, 0, 162, 1);
 
 	transform_.rotate_.y = 180.f;
